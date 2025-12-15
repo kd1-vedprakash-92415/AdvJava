e15e1301f8369ef491737f073b3b6480c64e70ce
Vedprakash Umare <madhurumare2003@gmail.com>
2025-12-15 18:24:47 +0530

    Assignment01

---
+++ Assignment01/assign01/src/main/java/assign01/HelloServlet.java
@@ -0,0 +1,32 @@
+package assign01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+
+public class HelloServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();	
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title> Hello </title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1> Hello </h1>");
+		out.println("<a href=index.html>Click to redirect</a>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+
+}
